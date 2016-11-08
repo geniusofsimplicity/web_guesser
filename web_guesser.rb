d9ba8d352e687e31324d5eb66ad59a5b2c8c4435
Pasha <genius.of.simplicity@gmail.com>
2016-11-08 19:52:08 +0100

    I4: Extensions

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,10 +1,23 @@
 require "sinatra"
 require "sinatra/reloader"
 
+class Tracking	
+	@@count = 5	
+	def self.use
+		@@count -= 1
+		if @@count < 1
+			@@count = 5			
+			return true
+		end
+		false
+	end
+end
+
 set :number, rand(101)
+set :set_new_number, Proc.new { number = rand(101) }
 
 helpers do
-	def check_guess(guess)
+	def check_guess(guess = 0)		
 		number = settings.number
 		message = "too high!" if (1..10).include?(guess - number)
 		message = "too low!" if (-10..-1).include?(guess - number)
@@ -17,8 +30,10 @@ end
 
 
 get "/" do
-	guess = params["guess"].scan(/\d*/).select{|v| v.size > 0}[0].to_i
-	message = check_guess(guess)
+	guess = params["guess"].scan(/\d*/).select{|v| v.size > 0}[0].to_i if params["guess"]
+	message = check_guess(guess) if params["guess"]	
 	number = settings.number
-	erb :index, {locals: {number: number, message: message}}	
-end 
+	reset = (Tracking.use && message != "correct!") || message == "correct!"
+	settings.number = settings.set_new_number if reset
+	erb :index, {locals: {number: number, message: message, reset: reset}}	
+end
